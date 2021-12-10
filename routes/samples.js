e62e0019dbc39af59228e76202e77d8411bbb127
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-10 11:38:42 +0900

    first commit

---
+++ routes/samples.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+
+/* サンプルAPI①
+ * http://localhost:3000/samples にGETメソッドのリクエストを投げると、
+ * JSON形式で文字列を返す。
+ */
+router.get("/", function (req, res, next) {
+  var param = {
+    sample: "これはサンプルAPIです",
+    array: [1, 2, 3, 4, 4, 5, 6, 7, 7, 8],
+    test: [test, test, test],
+  };
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.json(param);
+});
+
+/* サンプルAPI②
+ * http://localhost:3000/samples/hello にGETメソッドのリクエストを投げると、
+ * JSON形式で文字列を返す。
+ */
+router.get("/hello", function (req, res, next) {
+  var param = { result: "Hello World !" };
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.send(param);
+});
+
+module.exports = router;
