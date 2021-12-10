84124b2de154dbbfc797bcf54a182c43bc9aa253
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-10 14:56:47 +0900

    test

--- routes/samples.js
+++ routes/samples.js
@@ -5,7 +5,7 @@ var router = express.Router();
  * http://localhost:3000/samples にGETメソッドのリクエストを投げると、
  * JSON形式で文字列を返す。
  */
-router.get("/", function (req, res, next) {
+router.get("/test", function (req, res, next) {
   var param = {
     sample: "これはサンプルAPIです",
     array: [1, 2, 3, 4, 4, 5, 6, 7, 7, 8],
