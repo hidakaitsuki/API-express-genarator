ac7bd029b8e0528e7c1513c94e8fed9d21a4b8f1
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-11 15:45:13 +0900

    test

--- routes/samples.js
+++ routes/samples.js
@@ -17,7 +17,7 @@ router.get("/", function (req, res) {
   };
     res.header("Content-Type", "application/json; charset=utf-8");
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.send(JSON.parse(param));
+  res.send(param);
 });
 
 /* サンプルAPI②
