36b1ef21184d41917d73a65d7deb2b133c9e43fe
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-13 11:03:53 +0900

    test

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use("/samples", samples);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.json());
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
