9e8580f14d5c5925ee794616e02181550fb3919b
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-14 10:37:30 +0900

    test

--- routes/samples.js
+++ routes/samples.js
@@ -10,12 +10,12 @@ var router = express.Router();
  *
  */
 router.post("/", function (req, res) {
-  var param = {
-    sample: "これはサンプルAPIです",
-    array: [1, 2, 3, 4, 4, 5, 6, 7, 7, 8],
-    test: ["test", "test", "test"],
-  };
-  res.header("Content-Type", "application/json; charset=utf-8");
+//   var param = {
+//     sample: "これはサンプルAPIです",
+//     array: [1, 2, 3, 4, 4, 5, 6, 7, 7, 8],
+//     test: ["test", "test", "test"],
+//   };
+//   res.header("Content-Type", "application/json; charset=utf-8");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(req.body);
 });
