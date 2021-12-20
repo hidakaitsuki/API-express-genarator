844ae12e68d8675dbc468102de4de0518a40706c
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-20 13:37:35 +0900

    test

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 // ここの下に上で作ったAPI
 app.use("/samples", samples);
 
-app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
