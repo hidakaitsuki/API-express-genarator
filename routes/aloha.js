130b48013ea7cac7c1cfe23ab107df3107eafea3
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-24 11:50:00 +0900

    test

--- routes/aloha.js
+++ routes/aloha.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var router = express.Router();
 // https://api-rks-generator.herokuapp.com/aloha
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  // herokuに登録した環境変数をもってくる「process.env.設定したkey」でもってこれる
+  `mongodb+srv://${process.env.NAME}:${process.env.PASS}@cluster0.bwr5d.mongodb.net/login?retryWrites=true&w=majority`,
+  () => {
+    console.log("mongoDBに接続しました");
+  }
+);
 
 router.post("/", function (req, res) {
   const test = req.body;
@@ -10,3 +19,5 @@ router.post("/", function (req, res) {
   // res.set("Access-Control-Allow-Origin", "*");
   res.send("test");
 });
+
+module.exports = router;
