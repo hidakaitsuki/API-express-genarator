33d985cba8d1d2ede762c044b1c3642f8c562954
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-24 11:59:10 +0900

    test

--- routes/aloha.js
+++ routes/aloha.js
@@ -14,7 +14,7 @@ const itemSchema = mongoose.Schema({ item: Array });
 const itemmodel = mongoose.model("items",itemSchema);
 router.post("/", function (req, res) {
   const items = new itemmodel();
-  items=req.body
+  items.item=req.body
   items.save()
 });
 
