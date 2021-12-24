f132706b54c855e746ba4b5ce5ede728bdc7cc3e
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-24 11:56:54 +0900

    test

--- routes/aloha.js
+++ routes/aloha.js
@@ -5,19 +5,16 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   // herokuに登録した環境変数をもってくる「process.env.設定したkey」でもってこれる
-  `mongodb+srv://${process.env.NAME}:${process.env.PASS}@cluster0.bwr5d.mongodb.net/login?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.NAME}:${process.env.PASS}@cluster0.bwr5d.mongodb.net/aloha?retryWrites=true&w=majority`,
   () => {
     console.log("mongoDBに接続しました");
   }
 );
-
+const itemmodel = mongoose.model("items");
 router.post("/", function (req, res) {
-  const test = req.body;
-  console.log(test);
-  // console.dir("bodyの中身" + JSON.stringify(req));
-  // res.setHeader("Content-Type", "application/json; charset=utf-8");
-  // res.set("Access-Control-Allow-Origin", "*");
-  res.send("test");
+  const items = new itemmodel();
+  items=req.body
+  items.save()
 });
 
 module.exports = router;
