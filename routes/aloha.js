b8fa2b52ef0cb31306ce051cbd49bada02358e25
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-26 16:36:55 +0900

    test

--- routes/aloha.js
+++ routes/aloha.js
@@ -68,12 +68,15 @@ const registerSchema = mongoose.Schema({
 });
 router.post("/register", function (req, res) {
   const registermodel = mongoose.model("register", registerSchema);
-
+  const totalcount = 0;
+  registermodel.countDocuments(function (err, result) {
+    totalcount = result;
+  });
   registermodel.find({ email: req.body.email }, function (err, result) {
     if (result.length === 0) {
       //   IDを自動採番するために今あるデータ数を取得
       const register = new registermodel();
-      register.id = 1;
+      register.id = totalcount;
       register.name = req.body.name;
       register.email = req.body.email;
       register.password = req.body.password;
