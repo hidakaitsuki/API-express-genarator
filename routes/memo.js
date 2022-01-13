f024870a04da1488b24fa33b774d620379f821c0
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2022-01-13 14:57:24 +0900

    test

--- routes/memo.js
+++ routes/memo.js
@@ -26,7 +26,7 @@ router.get("/register", function (req, res) {
   registermodel.find({}, function (err, result) {
     res.send(
       result.sort(function (a, b) {
-        a.id < b.id ? -1 : 1;
+       return a.id < b.id ? -1 : 1;
       })
     );
   });
