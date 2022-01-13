bbe62c6672b50e1618f4c78e88156a8f7f6c6e71
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2022-01-13 11:23:19 +0900

    test

--- routes/memo.js
+++ routes/memo.js
@@ -23,9 +23,9 @@ router.post("/register", function (req, res) {
     );
   const registermodel = mongoose.model("registers", userSchema);
   const totalcount = 0;
-  //   registermodel.find().count(function (err, result) {
-  //     totalcount = result;
-  //   });
+    registermodel.find().count(function (err, result) {
+      totalcount = result;
+    });
   const register = new registermodel();
   register.id = totalcount + 1;
   register.name = req.body.name;
